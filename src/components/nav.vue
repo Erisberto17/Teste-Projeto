fd448c31d9250723c2968000ea6ebbe32160f9d9
Eriberto <erislo2001@gmail.com>
2022-11-27 18:50:43 -0300

    nav

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -2,14 +2,16 @@
     <nav>
         <h1>CAMPANHA</h1>
         <div class="opt">
-            <a href=""><img src="../Imgs/Search.png" alt="Search"></a>
-            <a href=""><img src="../Imgs/Bell.png" alt="Bell"></a>
-            <a href=""><img src="../Imgs/chat.png" alt="Chat"></a>
-            <a href=""><img src="../Imgs/share.png" alt="Share"></a>
-            <a href=""><div id="user">
-                <img src="../Imgs/user.png" alt="User">
-                <p>Admin</p>
-            </div></a>
+            <a href="" class="icons"><img src="../Imgs/Search.png" alt="Search"></a>
+            <a href="" class="icons"><img src="../Imgs/Bell.png" alt="Bell"></a>
+            <a href="" class="icons"><img src="../Imgs/chat.png" alt="Chat"></a>
+            <a href="" class="icons"><img src="../Imgs/share.png" alt="Share"></a>
+            <div id="user">
+                <a href="" class="icons">
+                    <img src="../Imgs/user.png" alt="User">
+                Admin
+                </a>
+            </div>
 
         </div>
     </nav>
@@ -20,24 +22,49 @@ export default {
 }
 </script>
 <style scoped>
+    *{
+        font-family: helvetica;
+    }
     img{
         max-width: 20px;
-        min-height: 25px;
+        height: 25px;
     }
     nav{
         height: 250px;
         display: flex;
-
         justify-content: space-between;
         padding: 10px 20px;
     }
     nav .opt{
         padding: 15px;
-        margin: 0 15px;
+        margin: 0 5px;
+        display: flex;
+        
+    }
+    nav .opt .icons{
+        margin: 0 10px;
+        text-decoration: none;
+        color: rgb(118, 118, 118);
+        height: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
     }
     nav .opt #user{
         background-color: rgb(238, 238, 238);
         border-radius: 5px;
-        display: flex-box;
+        height: 30px;
+        padding: 0 4px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+        
+
+    }
+    nav .opt #user img{
+        height: 20px; width: 15px;
+        margin-right: 8px;
     }
 </style>
