9e1f22017eadf8b0ec2cd6b154e555a25a108b35
Eriberto <erislo2001@gmail.com>
2022-11-26 22:35:22 -0300

    Inicio

---
+++ src/components/nav.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav>
+        <h1>Nav</h1>
+    </nav>
+</template>
+<script>
+export default {
+    name:"Nav"
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <main>
+    <Nav/>
+  </main>
+</template>
+<script>
+ import Nav from "../components/nav.vue"
+ 
+export default {
+  name:"HomeView",
+  components:{
+    Nav
+  }
+}
+</script>
